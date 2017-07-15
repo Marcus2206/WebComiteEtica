b26d69f7d65da3c393858496eab0b5ed4450ded8
rasec <rasec@192.168.1.5>
2017-07-14 22:03:42 -0500

    Merge origin/master

--- web/common/controllers/generalController.js
+++ web/common/controllers/generalController.js
@@ -1,28 +1,34 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var app=angular.module("app");
 
 app.controller('fechaController', function ($scope) {
     $scope.fechaHoy = new Date();
 });
 
 app.controller("subirController", function ($scope, $http,$log) {
-    $scope.archivoPS=null;
-    $scope.subirArch = function () {
-        var obj = document.getElementById("fichero").files;
-        $log.log(obj);
-        $http.post("http://localhost:8080/RestComiteEtica" + '/api/greet', obj)
-                .then(function (data) {
-                    alert("Exito");
-                    $log.log(data);
-                }).catch(function (data) {
-                    alert("error");
-                    $log.log(data);
-        });
-    }
+//    $scope.archivoPS=null;
+//    $scope.subirArch = function () {
+//        var obj = document.getElementById("fichero").files;
+//        $log.log(obj);
+//        $http.post("http://localhost:8080/RestComiteEtica" + '/api/greet', obj)
+//                .then(function (data) {
+//                    alert("Exito");
+//                    $log.log(data);
+//                }).catch(function (data) {
+//                    alert("error");
+//                    $log.log(data);
+//        });
+//    };
     
+        $scope.envia=function(){
+            $log.log("entra");
+            $http.post("http://localhost:8080/RestComiteEtica/file/Nada")
+                    .then(function (data){
+                        alert("listo");
+                    })
+                    .catch(function (data){
+                        alert("error");
+                    });
+        };
     
-})
+});
 
