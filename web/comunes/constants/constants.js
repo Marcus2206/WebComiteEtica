3a861b848849f4af6bc886589f48b737c929cc4b
rasec <rasec@192.168.1.5>
2017-10-02 23:18:40 -0500

    últimos cambios en validación

--- web/comunes/constants/constants.js
+++ web/comunes/constants/constants.js
@@ -1,4 +1,4 @@
 var app=angular.module("app");
 
-app.constant("baseUrl", "https://etica.prisma.org.pe:8181/RestComiteEtica");
-app.constant("UrlOrigen", "https://etica.prisma.org.pe:8181/WebComiteEtica");
+app.constant("baseUrl", "https://localhost:8181/RestComiteEtica");
+app.constant("UrlOrigen", "https://localhost:8181/WebComiteEtica");
